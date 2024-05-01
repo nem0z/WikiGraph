6d2886ad2defda87a15c7f1210691f2448644fe8
nem0z <maxime.bernard.dev@gmail.com>
2024-05-01 16:10:54 +0200

    test: write broker example

--- src/main.go
+++ src/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	mqbroker "github.com/nem0z/WikiGraph/broker"
+)
+
+func handle(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("WikiGraph")
+	fmt.Println("WikiGraph bis")
+
+	err := godotenv.Load()
+	handle(err)
+
+	RMQ_URI := os.Getenv("RABBITMQ_URI")
+	broker, err := mqbroker.New(RMQ_URI, "testing", "unprocessed_articles", "relations")
+	handle(err)
+
+	err = broker.Publish("testing", []byte("Test publishing"))
+	handle(err)
+
+	chMsg, err := broker.GetConsumer("testing")
+	handle(err)
+
+	for msg := range chMsg {
+		fmt.Println(string(msg.Body))
+	}
+
+	select {}
 }
