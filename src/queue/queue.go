d25199edbd08c4daeddd70865c9aac63d0ea407d
nem0z <maxime.bernard.dev@gmail.com>
2024-05-18 16:55:10 +0200

    feat: implement a queue refill system

---
+++ src/queue/queue.go
@@ -0,0 +1,30 @@
+package queue
+
+import (
+	mqbroker "github.com/nem0z/WikiGraph/broker"
+	"github.com/nem0z/WikiGraph/database"
+)
+
+const QueueSize uint = 1000
+
+type Queue struct {
+	broker *mqbroker.Broker
+	db     *database.DB
+}
+
+func New(broker *mqbroker.Broker, db *database.DB) *Queue {
+	return &Queue{broker, db}
+}
+
+func (q *Queue) Fill() error {
+	links, err := database.GetUnprocessedArticleLinks(q.db, QueueSize)
+	if err != nil {
+		return err
+	}
+
+	for _, link := range links {
+		q.broker.Publish(mqbroker.UnprocessedUrlQueue, []byte(link))
+	}
+
+	return nil
+}
