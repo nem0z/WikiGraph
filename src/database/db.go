d3d0bf961381bbf1c4be4f26d8154a848b74e1c3
nem0z <maxime.bernard.dev@gmail.com>
2024-05-31 17:47:34 +0200

    feat: add cache interface to db

--- src/database/db.go
+++ src/database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 
+	"github.com/nem0z/WikiGraph/app/entity"
 	redispkg "github.com/nem0z/WikiGraph/database/redis"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -10,10 +11,11 @@ import (
 
 type DB struct {
 	*sql.DB
-	cache *redispkg.Redis
+	cache           Cache
+	onInsertArticle func(article *entity.Article)
 }
 
-func New(config *Config) (*DB, error) {
+func New(config *Config, onInsertArticle func(article *entity.Article)) (*DB, error) {
 	db, err := sql.Open("mysql", config.Uri())
 	if err != nil {
 		return nil, err
@@ -23,6 +25,6 @@ func New(config *Config) (*DB, error) {
 		return nil, err
 	}
 
-	err = Init(db, config.InitScriptPath)
-	return &DB{db, redispkg.New(config.RedisConfig)}, err
+	cache := redispkg.New(config.RedisConfig)
+	return &DB{db, cache, onInsertArticle}, Init(db, config.InitScriptPath)
 }
